3776e5d2d32b9540d39dff47a5e63c76de83b320
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2024-02-28 13:06:17 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 179ms, Memory: 14MB, Status: Passed - CodeTree

--- 240228/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.js
+++ 240228/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.js
@@ -1,11 +1,11 @@
-function isMultipleOfFive(num) {
+function print(num) {
     const numArr = num.toString().split('');
     const sum = +numArr[0] + +numArr[1];
-    const msg = !(sum % 5) && !(sum  % 2) ? 'Yes' : 'No';
+    const msg = !(sum % 5) && !(num % 2) ? 'Yes' : 'No';
     console.log(msg);
 }
 
 
 const fs = require('fs');
 let rowNum = Number(fs.readFileSync(0).toString().trim());
-isMultipleOfFive(rowNum);
+print(rowNum);
